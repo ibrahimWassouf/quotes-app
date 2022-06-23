27394b50dd60a2401ed2146b91fb262d8104f603
Ibrahim Wassouf <ib981347@dal.ca>
2022-06-23 12:33:53 -0300

    started login, logout, and signup functionality

--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse, resolve
+
+from .forms import CustomUserCreationForm
+from .views import SignUpView
 
 # Create your tests here.
 
@@ -31,3 +35,29 @@ class CustomUserTests(TestCase):
         self.assertTrue(user.is_active)
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
+
+
+class SignupTests(TestCase):
+
+    #creates url object and its response template
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    #tests that the url works and the correct template is used
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'signup.html')
+        self.assertContains(self.response, 'Sign up')
+
+    #tests to see whether response template has correct form and protection
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    #tests to see whether correct view is used
+    def test_signup_iew(self):
+        view = resolve('/accounts/signup/')
+        self.assertEqual(view.func.__name__,
+        SignUpView.as_view().__name__)
