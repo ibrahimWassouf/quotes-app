dd5e5e4ca940b9c2ddf847678919602aa7c23088
Ibrahim Wassouf <ib981347@dal.ca>
2022-06-23 13:37:55 -0300

    added staticfiles directory and implemented bootstrap and crispy forms for future use

--- quotes/settings.py
+++ quotes/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #third-party
+    'crispy_forms',
+
     #local apps
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
@@ -126,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -136,3 +141,5 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
