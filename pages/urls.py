397c606500488f9336b89d7dde43589eee551a20
Ibrahim Wassouf <ib981347@dal.ca>
2022-08-15 16:35:14 -0300

    created pages to display list of quotes and each individual quote

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, QuoteDetailView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('quote/<int:pk>', QuoteDetailView.as_view(), name="quote_detail"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
+from quotations.models import BookQuote
 
 # Create your views here.
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = BookQuote
     template_name = 'home.html'
+
+class QuoteDetailView(DetailView):
+    model = BookQuote
+    template_name = 'quote_detail.html'
