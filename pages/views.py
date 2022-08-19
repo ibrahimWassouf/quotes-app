0725a48a9c1db6845a6b752c4ef87a7c46c42cc6
Ibrahim Wassouf <ib981347@dal.ca>
2022-08-19 19:35:00 -0300

    added signal that creates collection called All Quotes for each new user created

--- pages/views.py
+++ pages/views.py
@@ -1,30 +1,42 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from quotations.models import BookQuote
+from quotations.models import BookQuote, Collections
+from users.models import CustomUser
 from django.urls import reverse_lazy
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your views here.
 
 class HomePageView(ListView):
     model = BookQuote
     template_name = 'home.html'
 
-class QuoteDetailView(DetailView):
+class QuoteDetailView(LoginRequiredMixin, DetailView):
     model = BookQuote
     template_name = 'quote_detail.html'
 
-class QuoteCreateView(CreateView):
+class QuoteCreateView(LoginRequiredMixin, CreateView):
     model = BookQuote
     template_name = 'quote_new.html'
     fields = ['title', 'author', 'quote']
 
-class QuoteUpdateView(UpdateView):
+class QuoteUpdateView(LoginRequiredMixin, UpdateView):
     model = BookQuote
     template_name = 'quote_edit.html'
     fields = ['title', 'author', 'quote']
 
-class QuoteDeleteView(DeleteView):
+class QuoteDeleteView(LoginRequiredMixin, DeleteView):
     model = BookQuote
     template_name='quote_delete.html'
     success_url = reverse_lazy('home')
+
+
+@receiver(post_save, sender=CustomUser)
+def createAllCollections(sender, instance, created, **kwargs):
+    if created:
+        allCollections = Collections(title="All Quotes")
+        allCollections.save()
+    else:
+        pass
