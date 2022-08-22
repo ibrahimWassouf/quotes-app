8fe4bc6a364249ce0588dc6a9c32255c5ca96b4e
Ibrahim Wassouf <ib981347@dal.ca>
2022-08-22 20:23:55 -0300

    fixed bug where creator was not set for collections for each user

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from quotations.models import BookQuote, Collections
@@ -26,15 +26,23 @@ class QuoteCreateView(LoginRequiredMixin, CreateView):
         form.instance.creator = self.request.user
         return super().form_valid(form)
 
-class QuoteUpdateView(LoginRequiredMixin, UpdateView):
+class QuoteUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = BookQuote
     template_name = 'quote_edit.html'
     fields = ['title', 'author', 'quote']
 
-class QuoteDeleteView(LoginRequiredMixin, DeleteView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.creator == self.request.user.username
+
+class QuoteDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = BookQuote
     template_name='quote_delete.html'
     success_url = reverse_lazy('home')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.creator == self.request.user.username
 
 class CollectionsList(ListView):
     model = Collections
@@ -50,11 +58,11 @@ class CollectionDetailView(DetailView):
 @receiver(post_save, sender=CustomUser)
 def createAllCollections(sender, instance, created, **kwargs):
     if created:
-        allCollections = Collections(title="All Quotes")
+        allCollections = Collections(title="All Quotes", creator = instance.username )
         allCollections.save()
 
 @receiver(post_save, sender=BookQuote)
 def quoteCreated(sender, instance, created, **kwargs):
     if created:
-        allCollections = Collections.objects.get(title="All Quotes")
+        allCollections = Collections.objects.get(creator=instance.creator)
         allCollections.quotes.add(instance)
