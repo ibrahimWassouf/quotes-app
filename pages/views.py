7b168ff641363f150c5452bb589abb300dc1006f
Ibrahim Wassouf <ib981347@dal.ca>
2022-08-23 13:09:24 -0300

    added new permission where each user only sees their own quotes

--- pages/views.py
+++ pages/views.py
@@ -13,6 +13,11 @@ class HomePageView(ListView):
     model = BookQuote
     template_name = 'home.html'
 
+    #updates the query set to include only the quotes created by the user
+    def get_queryset(self):
+        quoteList = BookQuote.objects.all().filter(creator=self.request.user)
+        return quoteList
+
 class QuoteDetailView(LoginRequiredMixin, DetailView):
     model = BookQuote
     template_name = 'quote_detail.html'
@@ -22,6 +27,7 @@ class QuoteCreateView(LoginRequiredMixin, CreateView):
     template_name = 'quote_new.html'
     fields = ['title', 'author', 'quote']
 
+    #automatically fills the creator field with the user's name
     def form_valid(self, form):
         form.instance.creator = self.request.user
         return super().form_valid(form)
@@ -31,6 +37,7 @@ class QuoteUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name = 'quote_edit.html'
     fields = ['title', 'author', 'quote']
 
+    #ensures only the correct user -the creator- has access to the view
     def test_func(self):
         obj = self.get_object()
         return obj.creator == self.request.user.username
@@ -48,19 +55,24 @@ class CollectionsList(ListView):
     model = Collections
     template_name = 'collections_list.html'
 
+    def get_queryset(self):
+        collectionList = Collections.objects.all().filter(creator= self.request.user)
+        return collectionList
+
 class CollectionDetailView(DetailView):
     model = Collections
     template_name= 'collection_detail.html'
 
 
 
-
+#creates a collection called 'All Quotes' for each newly signed up user
 @receiver(post_save, sender=CustomUser)
 def createAllCollections(sender, instance, created, **kwargs):
     if created:
         allCollections = Collections(title="All Quotes", creator = instance.username )
         allCollections.save()
 
+#adds each newly made quote into the user's specific 'all quotes' collection
 @receiver(post_save, sender=BookQuote)
 def quoteCreated(sender, instance, created, **kwargs):
     if created:
