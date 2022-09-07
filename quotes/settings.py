0dabfcf509bd6e5d289d31bdb2bcb46cf1833ee7
Ibrahim Wassouf <ib981347@dal.ca>
2022-09-07 20:06:23 -0300

    getting ready to deply

--- quotes/settings.py
+++ quotes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-*gi8eor6*5eddjp%#w!fhh&gvf-98*5t&rx=w6)q38tt_r!x*k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', 'https://quotes-web-app.azurewebsites.net']
 
 
 # Application definition
