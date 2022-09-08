4806cb2012bb3006c0fdd0d9c177d450d8ad7c9f
Ibrahim Wassouf <ib981347@dal.ca>
2022-09-07 21:08:30 -0300

    added better security

--- quotes/settings.py
+++ quotes/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*gi8eor6*5eddjp%#w!fhh&gvf-98*5t&rx=w6)q38tt_r!x*k'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', 'quotes-web-app.azurewebsites.net']
 
